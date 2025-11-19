33a3e49be79f19aea394f9e2674c274241611da7
justEstif <95101485+justEstif@users.noreply.github.com>
2025-11-19 10:22:42 -0500

    feat: setup better auth

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { sveltekitCookies } from 'better-auth/svelte-kit'
+import { getRequestEvent } from '$app/server'
+import { db } from './db'
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, { provider: 'sqlite' }),
+	plugins: [sveltekitCookies(getRequestEvent)],
+	emailAndPassword: { enabled: true  }
+})

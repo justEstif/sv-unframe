cd313838a72acaf2a9e2c0d6589424b733bc2a4c
estifanos <95101485+justEstif@users.noreply.github.com>
2025-11-20 08:12:00 -0500

    feat: continue supporting the better auth error messages

--- src/lib/api/auth.remote.ts
+++ src/lib/api/auth.remote.ts
@@ -1,7 +1,8 @@
-import { redirect } from "@sveltejs/kit";
+import { isRedirect, redirect } from "@sveltejs/kit";
 import { form, getRequestEvent, query } from "$app/server";
 import { auth } from "$lib/server/auth";
 import { z } from "zod";
+import { APIError } from "better-auth";
 
 const signupSchema = z.object({
   name: z.string().min(4),
@@ -14,21 +15,55 @@ const loginSchema = z.object({
   password: z.string().min(8),
 });
 
-export const signup = form(signupSchema, async (user) => {
-  await auth.api.signUpEmail({ body: user });
-  redirect(307, "/dashboard");
+export const signup = form(signupSchema, async (user, invalid) => {
+  try {
+    await auth.api.signUpEmail({ body: user });
+    redirect(307, "/dashboard");
+  } catch (error) {
+    if (isRedirect(error)) {
+      throw error;
+    }
+    if (error instanceof APIError) {
+      console.log(error.message, error.status);
+      invalid.password(error.message);
+    }
+    throw error;
+  }
 });
 
-export const login = form(loginSchema, async (user) => {
+export const login = form(loginSchema, async (user, invalid) => {
   const { request } = getRequestEvent();
-  await auth.api.signInEmail({ body: user, headers: request.headers });
-  redirect(303, "/dashboard");
+  try {
+    await auth.api.signInEmail({ body: user, headers: request.headers });
+    redirect(303, "/dashboard");
+  } catch (error) {
+    if (isRedirect(error)) {
+      throw error;
+    } else if (error instanceof APIError) {
+      console.log(error.message, error.status);
+      invalid.password(error.message);
+    } else {
+      throw error;
+    }
+  }
 });
 
-export const signout = form(async () => {
+export const signout = form(async (_, invalid) => {
   const { request } = getRequestEvent();
-  await auth.api.signOut({ headers: request.headers });
-  redirect(303, "/");
+
+  try {
+    await auth.api.signOut({ headers: request.headers });
+    redirect(303, "/");
+  } catch (error) {
+    if (isRedirect(error)) {
+      throw error;
+    } else if (error instanceof APIError) {
+      console.log(error.message, error.status);
+      invalid(error.message);
+    } else {
+      throw error;
+    }
+  }
 });
 
 export const getUser = query(async () => {

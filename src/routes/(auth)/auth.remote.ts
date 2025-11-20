c908cc33be7e6a0b6f9c656305feba8635b5e330
justEstif <95101485+justEstif@users.noreply.github.com>
2025-11-20 07:20:14 -0500

    feat: continue with auth setup

---
+++ src/routes/(auth)/auth.remote.ts
@@ -0,0 +1,40 @@
+import { redirect } from "@sveltejs/kit";
+import { form, getRequestEvent, query } from "$app/server";
+import { auth } from "$lib/server/auth";
+import { z } from "zod";
+
+const signupSchema = z.object({
+  name: z.string().min(4),
+  email: z.email(),
+  password: z.string().min(8),
+});
+
+const loginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
+
+export const signup = form(signupSchema, async (user) => {
+  await auth.api.signUpEmail({ body: user });
+  redirect(307, `/admin`);
+});
+
+export const login = form(loginSchema, async (user) => {
+  const { request } = getRequestEvent();
+  await auth.api.signInEmail({ body: user, headers: request.headers });
+  redirect(303, "/admin");
+});
+
+export const signout = form(async () => {
+  const { request } = getRequestEvent();
+  await auth.api.signOut({ headers: request.headers });
+  redirect(303, "/");
+});
+
+export const getUser = query(async () => {
+  const { locals } = getRequestEvent();
+  if (!locals.user) {
+    redirect(307, "/auth/login");
+  }
+  return locals.user;
+});
